84fc55c07712005bd3cb89915f01435c3293ab00
Kene <kayyungx@gmail.com>
2023-08-28 16:49:24 +0100

    update producer service

--- src/kafka/producer.service.ts
+++ src/kafka/producer.service.ts
@@ -3,14 +3,16 @@ import {
   OnApplicationShutdown,
   OnModuleInit,
 } from '@nestjs/common';
-import { Kafka, Producer, ProducerRecord } from 'kafkajs';
+import { Kafka, Producer, ProducerRecord, Partitioners } from 'kafkajs';
 
 @Injectable()
 export class ProducerService implements OnModuleInit, OnApplicationShutdown {
   private readonly kafka = new Kafka({
     brokers: ['localhost:9092'],
   });
-  private readonly producer: Producer = this.kafka.producer();
+  private readonly producer: Producer = this.kafka.producer({
+    createPartitioner: Partitioners.LegacyPartitioner,
+  });
 
   async onModuleInit() {
     await this.producer.connect();
